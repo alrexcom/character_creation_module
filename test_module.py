bede061e5f90f866a5f2023bc4c17778dc7f3918
Alrexcom <mpstmprt@mail.ru>
2025-01-24 17:00:06 +0300

    Добавлена графика

--- test_module.py
+++ test_module.py
@@ -11,3 +11,11 @@ def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
+    try:
+        import main
+    except Exception as e:
+        assert False, (
+            'Не удалось запустить `main.py`. '
+            'Исправьте в нем ошибки:\n'
+            f'{e}'
+        )
